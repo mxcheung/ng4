d3b1c1c99e02e687e4662acb2ef91bea5f3fc5eb
max cheung <maxcheung@optusnet.com.au>
2017-10-09 19:45:41 +1100

    Add ball component variable message

--- soccer/src/app/ball/ball.component.ts
+++ soccer/src/app/ball/ball.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BallComponent implements OnInit {
 
+  message: String;
+  
   constructor() { }
 
   ngOnInit() {
+    this.message = 'hello';
   }
 
 }
