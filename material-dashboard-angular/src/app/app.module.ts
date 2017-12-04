a92e2c1cd4f13810e1bd55e7419cbb39d4386417
max cheung <maxcheung@optusnet.com.au>
2017-12-05 07:10:22 +1100

    Add form builder

--- material-dashboard-angular/src/app/app.module.ts
+++ material-dashboard-angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -40,6 +40,7 @@ RouterModule,
     AppRoutingModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- material-dashboard-angular/src/app/user-profile/user-profile.component.ts
+++ material-dashboard-angular/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter,  Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  // constructor(fb: FormBuilder) {
 
-  ngOnInit() {
+  /*
+      this.productForm = new FormGroup({
+        firstName: new FormControl()
+      });
+      */
+  constructor(@Inject(FormBuilder) fb: FormBuilder) {
+
+    this.form = fb.group({
+      name: fb.group({
+        first: ['Nancy', Validators.minLength(2)],
+        last: 'Drew',
+      }),
+      email: '',
+    });
+   /* 
+    this.userForm = new FormGroup({
+      firstName: new FormControl()
+    });
+    */
+  }
+
+  async ngOnInit() {
+  }
+
+  updateScreen() {
+    console.log(this);
+  }
+
+  inviteContact(formValues: any) {
+    console.log(formValues);
+    
+ 
   }
 
 }
