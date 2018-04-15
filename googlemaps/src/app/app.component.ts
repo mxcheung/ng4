e675326c3e37a3b6002b9da92717eea0f19f1468
max cheung <maxcheung@optusnet.com.au>
2018-04-15 19:44:13 +1000

    Google maps

---
+++ googlemaps/src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  latitude: any;
+  longitude: any;
+
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(-33.94292, 151.2393999),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)
+  }
+
+  setCenter() {
+    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+
+    let location = new google.maps.LatLng(this.latitude, this.longitude);
+
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      title: 'Got you!'
+    });
+
+    marker.addListener('click', this.simpleMarkerHandler);
+
+    marker.addListener('click', () => {
+      this.markerHandler(marker);
+    });
+  }
+
+  simpleMarkerHandler() {
+    alert('Simple Component\'s function...');
+  }
+
+  markerHandler(marker: google.maps.Marker) {
+    alert('Marker\'s Title: ' + marker.getTitle());
+  }
+}
